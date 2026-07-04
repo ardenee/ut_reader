f27bb02d5774d58972365badb657be5185927953
arden <arden@arden.ie>
2026-07-04 18:17:41 +0100

    fix: retain dependency invalidation support bootstrap

--- catalog/lib/CatalogAffectedDependencyRefreshService.php
+++ catalog/lib/CatalogAffectedDependencyRefreshService.php
@@ -1,11 +1,7 @@
 <?php
 declare(strict_types=1);
 
+require_once __DIR__ . '/CatalogSupport.php';
 require_once __DIR__ . '/../src/Application/Dependency/CatalogAffectedDependencyRefreshService.php';
 
-if (!class_exists('CatalogAffectedDependencyRefreshService', false)) {
-    class_alias(
-        \UnrealDb\Catalog\Application\Dependency\CatalogAffectedDependencyRefreshService::class,
-        'CatalogAffectedDependencyRefreshService'
-    );
-}
+class_alias('UnrealDb\\Catalog\\Application\\Dependency\\CatalogAffectedDependencyRefreshService', 'CatalogAffectedDependencyRefreshService');
