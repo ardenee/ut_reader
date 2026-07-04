664e60de481e00584aad8754eaa5b06ff6b6cac0
arden <arden@arden.ie>
2026-07-04 18:17:30 +0100

    fix: retain dependency resolver support bootstrap

--- catalog/lib/CatalogDependencyResolver.php
+++ catalog/lib/CatalogDependencyResolver.php
@@ -1,11 +1,7 @@
 <?php
 declare(strict_types=1);
 
+require_once __DIR__ . '/CatalogSupport.php';
 require_once __DIR__ . '/../src/Application/Dependency/CatalogDependencyResolver.php';
 
-if (!class_exists('CatalogDependencyResolver', false)) {
-    class_alias(
-        \UnrealDb\Catalog\Application\Dependency\CatalogDependencyResolver::class,
-        'CatalogDependencyResolver'
-    );
-}
+class_alias('UnrealDb\\Catalog\\Application\\Dependency\\CatalogDependencyResolver', 'CatalogDependencyResolver');
