7e58c01a404153c88bfc716d7cf890c281918c29
arden <arden@arden.ie>
2026-07-04 19:42:53 +0100

    chore: remove legacy catalog migration files

--- catalog/install_update_007_federation_child_downloads.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE ue_federation_transfer_jobs
-  ADD COLUMN remote_request_item_id BIGINT UNSIGNED NULL AFTER request_item_id;
-
-CREATE INDEX idx_ue_federation_transfer_remote_item ON ue_federation_transfer_jobs (remote_request_item_id);
