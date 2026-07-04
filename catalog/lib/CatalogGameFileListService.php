de5b638041a7b79116af34e42579087c24ba60ee
arden <arden@arden.ie>
2026-07-04 18:17:48 +0100

    fix: retain game list support bootstrap

--- catalog/lib/CatalogGameFileListService.php
+++ catalog/lib/CatalogGameFileListService.php
@@ -1,11 +1,7 @@
 <?php
 declare(strict_types=1);
 
+require_once __DIR__ . '/CatalogSupport.php';
 require_once __DIR__ . '/../src/Application/Catalog/CatalogGameFileListService.php';
 
-if (!class_exists('CatalogGameFileListService', false)) {
-    class_alias(
-        \UnrealDb\Catalog\Application\Catalog\CatalogGameFileListService::class,
-        'CatalogGameFileListService'
-    );
-}
+class_alias('UnrealDb\\Catalog\\Application\\Catalog\\CatalogGameFileListService', 'CatalogGameFileListService');
