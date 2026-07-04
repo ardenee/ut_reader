6c98e5192e0f61b8066711b1a86d2df148c5cecd
arden <arden@arden.ie>
2026-07-04 18:17:21 +0100

    fix: keep search compatibility bootstrap

--- catalog/lib/CatalogSearchService.php
+++ catalog/lib/CatalogSearchService.php
@@ -1,18 +1,8 @@
 <?php
 declare(strict_types=1);
 
+require_once __DIR__ . '/CatalogSupport.php';
 require_once __DIR__ . '/../src/Application/Search/CatalogSearchService.php';
 
-if (!class_exists('CatalogSearchService', false)) {
-    class_alias(
-        \UnrealDb\Catalog\Application\Search\CatalogSearchService::class,
-        'CatalogSearchService'
-    );
-}
-
-if (!class_exists('CatalogSearchUnavailableException', false)) {
-    class_alias(
-        \UnrealDb\Catalog\Application\Search\CatalogSearchUnavailableException::class,
-        'CatalogSearchUnavailableException'
-    );
-}
+class_alias('UnrealDb\\Catalog\\Application\\Search\\CatalogSearchService', 'CatalogSearchService');
+class_alias('UnrealDb\\Catalog\\Application\\Search\\CatalogSearchUnavailableException', 'CatalogSearchUnavailableException');
