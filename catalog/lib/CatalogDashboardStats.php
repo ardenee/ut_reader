313ede28991757c4ed8b3575bb9502ad7d8ef586
arden <arden@arden.ie>
2026-07-04 18:17:55 +0100

    fix: retain dashboard support bootstrap

--- catalog/lib/CatalogDashboardStats.php
+++ catalog/lib/CatalogDashboardStats.php
@@ -1,11 +1,7 @@
 <?php
 declare(strict_types=1);
 
+require_once __DIR__ . '/CatalogSupport.php';
 require_once __DIR__ . '/../src/Application/Dashboard/CatalogDashboardStats.php';
 
-if (!class_exists('CatalogDashboardStats', false)) {
-    class_alias(
-        \UnrealDb\Catalog\Application\Dashboard\CatalogDashboardStats::class,
-        'CatalogDashboardStats'
-    );
-}
+class_alias('UnrealDb\\Catalog\\Application\\Dashboard\\CatalogDashboardStats', 'CatalogDashboardStats');
