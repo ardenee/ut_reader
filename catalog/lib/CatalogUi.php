dd73ad0d46bb93d6464dabe8973313727fbb6688
arden <arden@arden.ie>
2026-07-18 20:29:58 +0100

    ui: load catalog component autoloader

--- catalog/lib/CatalogUi.php
+++ catalog/lib/CatalogUi.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 
+require_once __DIR__ . '/../bootstrap/autoload.php';
 require_once __DIR__ . '/../src/Presentation/Ui/CatalogUi.php';
 
 class_alias('UnrealDb\\Catalog\\Presentation\\Ui\\CatalogUi', 'CatalogUi');
