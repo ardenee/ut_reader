7de14c78360093da5f01b063b629aee09f1b51bb
arden <arden@arden.ie>
2026-07-19 00:02:59 +0100

    ops: make worker lease duration configurable

--- catalog/bin/catalog-worker.php
+++ catalog/bin/catalog-worker.php
@@ -12,23 +12,24 @@ use UnrealDb\Catalog\Application\Jobs\JobWorker;
 use UnrealDb\Catalog\Infrastructure\Jobs\CatalogMaintenanceJobHandler;
 use UnrealDb\Catalog\Infrastructure\Persistence\PdoJobQueue;
 
-$options = getopt('', ['queue::', 'max-jobs::', 'sleep-ms::', 'worker-id::']);
-$queueName = (string)($options['queue'] ?? 'catalog');
+$options = getopt('', ['queue::', 'max-jobs::', 'sleep-ms::', 'worker-id::', 'lease-seconds::']);
+$application = catalog_bootstrap();
+$queueName = (string)($options['queue'] ?? ($application->config['queue']['name'] ?? 'catalog'));
 $maxJobs = max(1, min((int)($options['max-jobs'] ?? 1), 10000));
 $sleepMs = max(50, min((int)($options['sleep-ms'] ?? 1000), 60000));
 $workerId = (string)($options['worker-id'] ?? (gethostname() . ':' . getmypid()));
+$leaseSeconds = max(15, min((int)($options['lease-seconds'] ?? ($application->config['queue']['lease_seconds'] ?? 120)), 3600));
 
-$application = catalog_bootstrap();
 $queue = new PdoJobQueue($application->db);
 $worker = new JobWorker(
     $queue,
     [new CatalogMaintenanceJobHandler($application->db, $application->config)],
     $queueName,
     $workerId,
-    120
+    $leaseSeconds
 );
 
-$completed = 0;
+$processed = 0;
 for ($index = 0; $index < $maxJobs; $index++) {
     $result = $worker->runOne();
     fwrite(STDOUT, json_encode($result, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . PHP_EOL);
@@ -36,11 +37,11 @@ for ($index = 0; $index < $maxJobs; $index++) {
     if ($result['status'] === 'idle') {
         break;
     }
-    $completed++;
+    $processed++;
 
     if ($index + 1 < $maxJobs && $sleepMs > 0) {
         usleep($sleepMs * 1000);
     }
 }
 
-exit($completed >= 0 ? 0 : 1);
+exit($processed >= 0 ? 0 : 1);
