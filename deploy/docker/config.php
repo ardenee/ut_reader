8b34db4bad22d72b0396fb26aa0b1f19dff59be8
arden <arden@arden.ie>
2026-07-18 21:45:45 +0100

    security: configure container authentication limits

--- deploy/docker/config.php
+++ deploy/docker/config.php
@@ -29,6 +29,9 @@ $base['site_name'] = $env('UNREALDB_SITE_NAME', 'Unreal File Catalog');
 $base['storage_path'] = $env('UNREALDB_STORAGE_PATH', '/var/www/html/catalog/storage');
 $base['max_upload_bytes'] = $int('UNREALDB_MAX_UPLOAD_BYTES', 256 * 1024 * 1024, 1, PHP_INT_MAX);
 $base['auth']['remember_days'] = $int('UNREALDB_REMEMBER_DAYS', 30, 1, 365);
+$base['auth']['login_max_attempts'] = $int('UNREALDB_LOGIN_MAX_ATTEMPTS', 8, 3, 50);
+$base['auth']['login_window_seconds'] = $int('UNREALDB_LOGIN_WINDOW_SECONDS', 900, 60, 86400);
+$base['auth']['login_block_seconds'] = $int('UNREALDB_LOGIN_BLOCK_SECONDS', 900, 60, 86400);
 $base['queue']['name'] = $env('UNREALDB_QUEUE_NAME', 'catalog');
 $base['queue']['lease_seconds'] = $int('UNREALDB_QUEUE_LEASE_SECONDS', 120, 30, 3600);
 
