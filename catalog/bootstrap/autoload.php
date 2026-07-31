c5d6d13c4c2469145055ff585e14acf6f4ab5011
arden <arden@arden.ie>
2026-07-31 11:59:10 +0100

    Use non-buffering central error bootstrap

--- catalog/bootstrap/autoload.php
+++ catalog/bootstrap/autoload.php
@@ -31,5 +31,5 @@ declare(strict_types=1);
     });
 })();
 
-require_once dirname(__DIR__) . '/lib/CatalogSystemError.php';
+require_once dirname(__DIR__) . '/lib/CatalogSystemErrorSafe.php';
 catalog_system_error_register();
