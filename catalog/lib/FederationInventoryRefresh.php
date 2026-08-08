f8a9b9ccf5a5acf9d3aa57eb0bf2e77dcfe4712e
arden <arden@arden.ie>
2026-08-08 23:21:38 +0100

    refactor: keep inventory refresh compatibility bootstrap

--- catalog/lib/FederationInventoryRefresh.php
+++ catalog/lib/FederationInventoryRefresh.php
@@ -7,6 +7,8 @@
  */
 declare(strict_types=1);
 
+require_once __DIR__ . '/CatalogSupport.php';
+
 use UnrealDb\Catalog\Infrastructure\Federation\CatalogFederationInventoryRefreshService;
 
 /** @return array<string,mixed> */
