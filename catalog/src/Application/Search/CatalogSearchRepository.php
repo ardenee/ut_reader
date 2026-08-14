a8d4013d194e944ab34fcfb319746d9e7b876cd1
arden <arden@arden.ie>
2026-08-14 17:54:41 +0100

    Add catalog search repository port

---
+++ catalog/src/Application/Search/CatalogSearchRepository.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace UnrealDb\Catalog\Application\Search;
+
+interface CatalogSearchRepository
+{
+    /** @return list<array<string,mixed>> */
+    public function findFiles(string $query, int $limit = 200, ?int $gameId = null): array;
+}
