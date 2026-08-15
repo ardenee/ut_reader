aecf4053262ff5c10b347709387a67948b4cd81f
arden <arden@arden.ie>
2026-08-15 19:48:32 +0100

    Hide public UPK exception details

--- catalog/upks.php
+++ catalog/upks.php
@@ -65,6 +65,6 @@ try {
     if (!headers_sent()) {
         catalog_head('UPK package error');
     }
-    echo CatalogUi::alert('danger', $error->getMessage(), 'UPK package management could not be loaded.');
+    echo CatalogUi::alert('danger', catalog_exception_display_message($error), 'UPK package management could not be loaded.');
     catalog_foot();
 }
