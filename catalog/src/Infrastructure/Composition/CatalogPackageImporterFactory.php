011911c83cbd4fdb81900791597c4db22a0e09a5
arden <arden@arden.ie>
2026-08-15 13:00:17 +0100

    Compose dependency-inverted package importer

--- catalog/src/Infrastructure/Composition/CatalogPackageImporterFactory.php
+++ catalog/src/Infrastructure/Composition/CatalogPackageImporterFactory.php
@@ -11,18 +11,18 @@ namespace UnrealDb\Catalog\Infrastructure\Composition;
 
 use PDO;
 use UnrealDb\Catalog\Infrastructure\Import\CatalogFailedUploadPreserverAdapter;
+use UnrealDb\Catalog\Infrastructure\Import\CatalogPackageImporterAdapter;
 use UnrealDb\Catalog\Infrastructure\Import\CatalogVerifiedPackageDependencyCoordinator;
 use UnrealDb\Catalog\Infrastructure\Import\CatalogVerifiedPackageIdentityRepository;
 use UnrealDb\Catalog\Infrastructure\Import\CatalogVerifiedPackageInspector;
 use UnrealDb\Catalog\Infrastructure\Import\CatalogVerifiedPackagePublisher;
-use UnrealDb\Catalog\Infrastructure\Import\PdoCatalogPackageImporter;
 
 final class CatalogPackageImporterFactory
 {
     /** @param array<string,mixed> $config */
-    public static function create(PDO $db, array $config): PdoCatalogPackageImporter
+    public static function create(PDO $db, array $config): CatalogPackageImporterAdapter
     {
-        return new PdoCatalogPackageImporter(
+        return new CatalogPackageImporterAdapter(
             new CatalogVerifiedPackageInspector($db, $config),
             new CatalogVerifiedPackageIdentityRepository($db),
             new CatalogVerifiedPackagePublisher($db, $config),
