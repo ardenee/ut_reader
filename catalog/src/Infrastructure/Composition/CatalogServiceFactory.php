6282c194bc24f0713ae4e5da06d74ff6be27e1ef
arden <arden@arden.ie>
2026-08-15 13:00:58 +0100

    Compose profiled upload importer centrally

--- catalog/src/Infrastructure/Composition/CatalogServiceFactory.php
+++ catalog/src/Infrastructure/Composition/CatalogServiceFactory.php
@@ -15,7 +15,6 @@ use UnrealDb\Catalog\Application\Unverified\UnverifiedDuplicateCleanupService;
 use UnrealDb\Catalog\Application\Upload\ProfiledUploadService;
 use UnrealDb\Catalog\Infrastructure\Filesystem\NativeUnverifiedFileSystem;
 use UnrealDb\Catalog\Infrastructure\Import\CatalogFailedUploadPreserverAdapter;
-use UnrealDb\Catalog\Infrastructure\Import\PdoCatalogPackageImporter;
 use UnrealDb\Catalog\Infrastructure\Logging\CatalogUploadFailureLogger;
 use UnrealDb\Catalog\Infrastructure\Persistence\PdoProfiledUploadGameCatalog;
 use UnrealDb\Catalog\Infrastructure\Persistence\PdoUnverifiedRecordStore;
@@ -34,7 +33,7 @@ final class CatalogServiceFactory
     {
         return new ProfiledUploadService(
             new PdoProfiledUploadGameCatalog($this->db),
-            new PdoCatalogPackageImporter($this->db, $this->config),
+            CatalogPackageImporterFactory::create($this->db, $this->config),
             new CatalogUploadFailureLogger($this->db),
             new CatalogFailedUploadPreserverAdapter($this->config)
         );
