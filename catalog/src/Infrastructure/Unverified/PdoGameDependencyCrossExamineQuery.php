5d12906c36254d21ea22aea92239de1e7f14c04f
arden <arden@arden.ie>
2026-09-24 22:17:52 +0100

    Remove stale v3 runtime metadata assumptions

--- catalog/src/Infrastructure/Unverified/PdoGameDependencyCrossExamineQuery.php
+++ catalog/src/Infrastructure/Unverified/PdoGameDependencyCrossExamineQuery.php
@@ -95,7 +95,7 @@ final class PdoGameDependencyCrossExamineQuery
             . 'COUNT(DISTINCT l.file_id,l.import_index) missing_count,'
             . 'COUNT(DISTINCT l.file_id) owner_count '
             . 'FROM ue_dependency_links l '
-            . 'JOIN ue_file_metadata m ON m.file_id=l.file_id AND m.format_version=3 '
+            . 'JOIN ue_file_metadata m ON m.file_id=l.file_id AND m.format_version=' . \UnrealDb\Catalog\Infrastructure\Metadata\BlockedCompressedMetadataContainer::FORMAT_VERSION . ' '
             . 'JOIN ue_files owner ON owner.id=l.file_id AND owner.scan_status="verified" '
             . 'JOIN ue_terms pkg ON pkg.id=l.required_package_term_id '
             . 'WHERE owner.game_id=? AND l.status=0 '
@@ -394,7 +394,7 @@ final class PdoGameDependencyCrossExamineQuery
                 . 'CONVERT(required_class_package.value_prefix USING utf8mb4) required_class_package,'
                 . 'CONVERT(required_class_name.value_prefix USING utf8mb4) required_class_name '
                 . 'FROM ue_dependency_links l '
-                . 'JOIN ue_file_metadata owner_meta ON owner_meta.file_id=l.file_id AND owner_meta.format_version=3 '
+                . 'JOIN ue_file_metadata owner_meta ON owner_meta.file_id=l.file_id AND owner_meta.format_version=' . \UnrealDb\Catalog\Infrastructure\Metadata\BlockedCompressedMetadataContainer::FORMAT_VERSION . ' '
                 . 'JOIN ue_files owner ON owner.id=l.file_id AND owner.scan_status="verified" '
                 . 'JOIN ue_terms pkg ON pkg.id=l.required_package_term_id '
                 . 'JOIN ue_terms required_path ON required_path.id=l.required_object_term_id '
@@ -403,7 +403,7 @@ final class PdoGameDependencyCrossExamineQuery
                 . 'JOIN ue_files source ON source.id IN (' . $placeholders . ') '
                 . 'AND source.scan_status="verified" '
                 . 'AND source.package_name=CONVERT(pkg.value_prefix USING utf8mb4) COLLATE utf8mb4_unicode_ci '
-                . 'JOIN ue_file_metadata source_meta ON source_meta.file_id=source.id AND source_meta.format_version=3 '
+                . 'JOIN ue_file_metadata source_meta ON source_meta.file_id=source.id AND source_meta.format_version=' . \UnrealDb\Catalog\Infrastructure\Metadata\BlockedCompressedMetadataContainer::FORMAT_VERSION . ' '
                 . 'JOIN ue_export_lookup exports ON exports.file_id=source.id AND exports.path_hash=l.required_path_hash '
                 . 'WHERE owner.game_id=? AND l.status=0',
                 array_merge($chunk, [$targetGameId])
